eff1a02e1133a7618ee5736c32c574145d5f1551
Ismail Mohamed <im_077@hotmail.com>
2019-10-16 09:17:25 +0200

    added files

---
+++ app.js
@@ -0,0 +1,74 @@
+var todoList = {
+  todos: [],
+
+  displayTodos: function () {
+    
+    if (this.todos.length === 0) {
+      console.log('My todos are empty');
+    }
+    else {
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+        console.log('(x)', this.todos[i].todoText)
+        }
+        else{
+          console.log('()', this.todos[i].todoText)
+        }
+
+      }
+
+
+
+    }
+  },
+
+  addTodo: function (todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+
+  },
+  changeTodo: function (postion, todoText) {
+    this.todos[postion].todoText = todotext;
+    this.displayTodos;
+  },
+  deleteTodo: function (postion) {
+    this.todos.splice(postion, 1);
+    this.displayTodos();
+  },
+  toogleCompleted: function (postion) {
+    var todo = this.todos[postion];
+    todo.completed = !todo.completed
+    this.displayTodos();
+  },
+  toggleAll: function()
+  {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    // Get number of completed todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    
+    // Case 1: om allt är ture, gör det false.
+    if (completedTodos === totalTodos) 
+    {
+      for (var i = 0; i < totalTodos; i++ )
+      this.todos[i].completed = false;
+    }
+    else
+    {
+      for (var i = 0; i < totalTodos; i++){
+        this.todos[i].completed === true;
+      }
+    }
+    this.displayTodos();
+  }
+
+};
